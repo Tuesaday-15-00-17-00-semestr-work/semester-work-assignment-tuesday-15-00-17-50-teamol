d3df09ace66afbfa3aaab2bc59c80dfd4b7e95c9
CMoonyz <xlackoo@stuba.sk>
2024-12-10 20:46:24 +0100

    sdagff

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.semestral_work {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.semestral_work to javafx.fxml;
-    exports com.example.semestral_work;
-}
